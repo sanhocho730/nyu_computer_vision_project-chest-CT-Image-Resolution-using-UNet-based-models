03acf43c80f12e1007d27d0553c8d6a5c0f7f278
Euijae Kim <kimeuijae4@gmail.com>
2023-12-17 15:35:02 -0500

    cleanup runet (#3)

--- runet/common/loss.py
+++ runet/common/loss.py
@@ -1,18 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 20 19:56:57 2021
-
-@author: quentin
-"""
-
 import torchvision.models as models
 import torch
-import torch.nn as nn
-
 
 class VGGPerceptualLoss(torch.nn.Module):
-    def __init__(self, resize=True,device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
+    def __init__(self, device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
         super(VGGPerceptualLoss, self).__init__()
         blocks = []
         blocks.append(models.vgg16(pretrained=True).features[:4].to(device).eval())
@@ -22,33 +12,22 @@ class VGGPerceptualLoss(torch.nn.Module):
         for bl in blocks:
             for p in bl:
                 p.requires_grad = False
-        self.device=device
         self.blocks = torch.nn.ModuleList(blocks)
-        self.transform = torch.nn.functional.interpolate
-        self.mean = torch.nn.Parameter(torch.tensor([0.485, 0.456, 0.406]).view(1,3,1,1)).to(device)
-        self.std = torch.nn.Parameter(torch.tensor([0.229, 0.224, 0.225]).view(1,3,1,1)).to(device)
-        self.resize = resize
+        self.device=device
 
     def forward(self, input, target):
-        if input.shape[1] != 3:
-            input = input.repeat(1, 3, 1, 1).to(self.device)
-            target = target.repeat(1, 3, 1, 1).to(self.device)
-        input = (input-self.mean) / self.std
-        target = (target-self.mean) / self.std
-        if self.resize:
-            input = self.transform(input, mode='bilinear', size=(224, 224), align_corners=False)
-            target = self.transform(target, mode='bilinear', size=(224, 224), align_corners=False)
         loss = 0.0
         x = input
         y = target
         for block in self.blocks:
             x = block(x)
             y = block(y)
-            loss = loss + nn.MSELoss()(x, y)
+            loss = loss + torch.nn.MSELoss()(x, y)
         return loss
-    
+
+
 def PSNR(Img_pred, Img_true):
-    return 10 * torch.log10(torch.max(Img_pred)**2 / nn.MSELoss()(Img_pred,Img_true))
+    return 10 * torch.log10(torch.max(Img_pred)**2 / torch.nn.MSELoss()(Img_pred,Img_true))
 
 def SSIM(Img_pred, Img_true):
     L = torch.max(Img_true) - torch.min(Img_true)
--- runet/utils/formatter.py
+++ runet/utils/formatter.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 from common.constants import CHECKPOINTS_FOLDER
-
+from pytz import timezone
 
 def format_best_checkpoint_name():
-    now = datetime.now()
+    now = datetime.now(timezone('EST'))
     checkpoint_file = CHECKPOINTS_FOLDER + f'checkpoints_{now.strftime("%d%m%Y")}_{now.strftime("%H%M%S")}_best.pth'
     return checkpoint_file
 
 def format_current_checkpoint_name():
-    now = datetime.now()
+    now = datetime.now(timezone('EST'))
     checkpoint_file = CHECKPOINTS_FOLDER + f'checkpoints_{now.strftime("%d%m%Y")}_{now.strftime("%H%M%S")}_current.pth'
     return checkpoint_file
